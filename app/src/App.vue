2f783f41b5070cd75e1a5cafc326fab879e40036
juankp2278 <juankp2278@gmail.com>
2023-10-09 18:48:02 -0300

    iniciando prueba

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,7 +1,19 @@
 <template>
+  <ul>
+    <li>Quienes Somos</li>
+    <li>Menu</li>
+    <li>Contatos</li>
+    <li>Experiencias</li>
+  </ul>
+
+
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+  
 </template>
+
+
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
